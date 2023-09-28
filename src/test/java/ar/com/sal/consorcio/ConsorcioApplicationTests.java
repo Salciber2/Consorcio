fda2e32fdaf89d9ae325d035c3bd8754473e6374
Salciber <santiago.juego19@gmail.com>
2023-09-28 13:00:08 -0300

    Agregando proyecto vacío

---
+++ src/test/java/ar/com/sal/consorcio/ConsorcioApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.sal.consorcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsorcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
